80b0c003ec19c602ab185eaec85932861f6d287e
Lucas Souza <lucas.lunasouza@mail.utoronto.ca>
2017-01-21 02:34:58 -0500

    Handled login error responses

--- public/login.js
+++ public/login.js
@@ -3,19 +3,28 @@ var app = angular.module('app', []);
 app.controller("ctrl", function($scope, $http){
 	$scope.username = "";
 	$scope.password = "";
+	$scope.errorMessage = "";
 
 	$scope.login =  function(){
 		console.log("Username: " + $scope.username + "\nPassword: " + $scope.password);
 		$http.post("/login", {
 			username: $scope.username, 
 			password: $scope.password
+		}).then(function successCallback(response) {
+			$scope.errorMessage = "";
+		}, function errorCallback(response) {
+			$scope.errorMessage = "Incorrect username or password";
 		});
-	};
+	}
 	$scope.createUser =  function(){
 		console.log("Username: " + $scope.username + "\nPassword: " + $scope.password);
 		$http.post("/createUser", {
 			username: $scope.username, 
 			password: $scope.password
+		}).then(function successCallback(response) {
+			$scope.errorMessage = "";
+		}, function errorCallback(response) {
+			$scope.errorMessage = "Username already exists";
 		});
 	};
 });
